1f5e749ae05c5cdeb9890dc935eb030d60a89a23
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-08-13 15:13:08 -0500

    feat: :tada:first commit

---
+++ src/main/java/com/sebbas/primeraApi/PrimeraApiApplication.java
@@ -0,0 +1,13 @@
+package com.sebbas.primeraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraApiApplication.class, args);
+	}
+
+}
